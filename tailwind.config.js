7022e8ec5eed8b2e8a88cd5bd53e7b91a61a7d29
Kranti Prasad <krantiprasad07@gmail.com>
2021-12-07 22:40:42 +0530

    feat: set-up

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	future: {
+		// removeDeprecatedGapUtilities: true,
+		// purgeLayersByDefault: true,
+	},
+	mode: "jit",
+	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+};
